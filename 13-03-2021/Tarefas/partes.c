364959ca46293afb0ac0653248ad5e9ddf87bf5f
rahfaeu <rahfaeu@debian.rahfaeu>
2021-03-19 20:38:55 -0300

    Criação do arquivo partes.c

---
+++ 13-03-2021/Tarefas/partes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+    1.Implemente uma função que recebe um número real passado valor,
+    retorne a parte inteira e a parte fracionaria desse número.
+    Depois, implemente a função mainque chama essa função.Protótipo:
+
+    void partes (float num, int *inteiro, float *fracionaria);
+*/
+
+/* Protótipo da função */
+void partes(float num, int *inteiro, float *fracionaria);
+
+int main() {
+
+float num;
+int *pi;
+float *pf;
+int parte_inteira;
+float parte_fracionada;
+
+pi = &parte_inteira;
+pf = &parte_fracionada;
+
+printf("Digite um numero real: ");
+scanf("%f", &num);
+
+partes(num, pi, pf);
+printf("Numero real: %.4f\n", num);
+printf("Parte inteira: %d\n", parte_inteira);
+printf("Parte fracionada: %.4f\n", parte_fracionada);
+
+return 0;
+
+}
+
+void partes(float num, int *inteiro, float *fracionaria) {
+    *inteiro = (int) num;
+    *fracionaria = num - ((int) num);
+}
