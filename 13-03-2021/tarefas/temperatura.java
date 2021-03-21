c7c54e96fd9066356913b9fd0135f652ed7a549c
rahfaeu <rahfaeu@debian.rahfaeu>
2021-03-21 20:49:30 -0300

    Criação do arquivo temperatura.java

---
+++ 13-03-2021/tarefas/temperatura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class temperatura {
+    public static void main(String[] args) {
+        float cel = 0;
+        float fah = 0;
+
+        System.out.println("Digite a temperatura em Celsius:");
+        Scanner entrada;
+        entrada = new Scanner(System.in);
+        cel = entrada.nextFloat();
+        fah = 9 * cel / 5 + 32;
+
+        System.out.printf("Fahrenheit: %.1f", fah);
+
+        entrada.close();
+    }
+}
